b0bd16b2bfb46dacdaf98cc392ab05174b4d86cf
Andy McFee <andy.mcfee@gmail.com>
2013-04-07 14:59:22 +0200

    Yo Angular + bower angular-ui

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngPlaygroundApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
